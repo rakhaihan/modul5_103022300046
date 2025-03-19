42019aead6cd0d937a483be142285f1adfbcb551
rakhaihan <rakharaihan2244@gmail.com>
2025-03-19 07:10:56 +0700

    menambahkan class PemrosesData dengan method DapatkanNilaiTerbesar

---
+++ PemrosesData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_103022300046
+{
+    class PemrosesData
+    {
+
+
+        public static T DapatkanNilaiTerbesar<T>(T data1, T data2, T data3)
+        {
+            dynamic nilai1 = (dynamic)data1;
+            dynamic nilai2 = (dynamic)data2;
+            dynamic nilai3 = (dynamic)data3;
+
+            dynamic dataMax = nilai1;
+
+
+            if (nilai2 > dataMax)
+            {
+                dataMax = data2;
+            } else
+            {
+                if (nilai3 > dataMax)
+                {
+                    dataMax = data3;
+                }
+            }
+
+            return dataMax;
+        }
+
+
+       
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+namespace modul5_103022300046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int terbesar = PemrosesData.DapatkanNilaiTerbesar(10, 30, 22);
+
+            Console.WriteLine(terbesar);
+        }
+    }
+}
