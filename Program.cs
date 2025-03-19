d63dc174d8ae8cd2abc8417b097d2c6ce7c7ce2b
rakhaihan <rakharaihan2244@gmail.com>
2025-03-19 07:40:41 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+namespace modul5_103022300046
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            SimpleDataBase<int> db = new SimpleDataBase<int>();
+            //NIM 103022300046
+            db.AddNewData(30);
+            db.AddNewData(00);
+            db.AddNewData(46);
+
+            db.PrintAllData();
+
+        }
+    }
+}
